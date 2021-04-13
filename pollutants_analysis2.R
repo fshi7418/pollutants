cc2c7a4d27d7ac049600a0801530be1cc3d90e8b
fshi7418 <fshi7418@user.noreply.github.com>
2021-04-12 20:26:24 -0700

    proto finalised model

--- pollutants_analysis2.R
+++ pollutants_analysis2.R
@@ -1,7 +1,25 @@
 library(glmnet)
 library(corrplot)
 pollutants_original <- read.csv("pollutants.csv", header = TRUE)
+
+# drop the first column of indices
 pollutants <- pollutants_original[-1]
+# drop a white cell pct due to perfect linearity
+pollutants <- pollutants[!colnames(pollutants) %in% c('eosinophils_pct')]
+# standardize all POP covariates
+for (i in 1:11) {
+  i_colname <- paste0('POP_PCB', i)
+  pollutants[i_colname] <- (pollutants[[i_colname]] - mean(pollutants[[i_colname]])) / sd(pollutants[[i_colname]])
+}
+for (i in 1:3) {
+  i_colname <- paste0('POP_dioxin', i)
+  pollutants[i_colname] <- (pollutants[[i_colname]] - mean(pollutants[[i_colname]])) / sd(pollutants[[i_colname]])
+}
+for (i in 1:4) {
+  i_colname <- paste0('POP_furan', i)
+  pollutants[i_colname] <- (pollutants[[i_colname]] - mean(pollutants[[i_colname]])) / sd(pollutants[[i_colname]])
+}
+
 
 gender_list <- c('female', 'male')
 edu_list <- c('before_high_school', 'high_school', 'college', 'college_grad')
@@ -23,201 +41,115 @@ pollutants$edu_cat <- factor(education, levels=edu_list)
 pollutants$race_cat <- factor(race, levels=eth_list)
 pollutants$smokenow <- factor(smoke_now, levels=smoke_list)
 
-## Factors removed 
-pollutants_factorsRemoved <- pollutants[-c(27,28,29,32)]
-
-pollutants_factorsRemovedCor <- cor(pollutants_factorsRemoved)
-
-set.seed(57)
-
-N <- nrow(pollutants)
-sampleTrain <- sample(1:N, round(N*0.8,0), replace = FALSE)
-
-dataTrain <- pollutants[sampleTrain,]
-dataTest <- pollutants[-sampleTrain,]
-
-M0 <- lm(length ~ 1, data=pollutants)
-Mfull <- lm(length ~ ., data=pollutants)
-Mstart <- M0
-
-MfwdAIC <- step(object = M0, scope = list(lower = M0, upper = Mfull), 
-                trace = FALSE, direction = "forward", k = 2)
-MfwdBIC <- step(object = M0, scope = list(lower = M0, upper = Mfull), 
-                trace = FALSE, direction = "forward", k = log(N))
-MbckAIC <- step(object = Mfull, scope = list(lower = M0, upper = Mfull), 
-                trace = FALSE, direction = "backward", k = 2)
-MbckBIC <- step(object = Mfull, scope = list(lower = M0, upper = Mfull), 
-                trace = FALSE, direction = "backward", k = log(N))
-MstepAIC <- step(object = Mstart, scope = list(lower = M0, upper = Mfull), 
-                trace = FALSE, direction = "both", k = 2)
-MstepBIC <- step(object = Mstart, scope = list(lower = M0, upper = Mfull), 
-                trace = FALSE, direction = "both", k = log(N))
-
-mspe <- function(model, df, y) {
-  model.res <- y - predict(model, newdata=df)
-  return(mean(model.res^2))
-}
-
-# cross validation
-kfold <- 12
-pollutants$index <- rep(1:kfold, each=N/kfold)
-
-MfwdAIC.mspe <- rep(NA, kfold)
-MfwdBIC.mspe <- rep(NA, kfold)
-MbckAIC.mspe <- rep(NA, kfold)
-MbckBIC.mspe <- rep(NA, kfold)
-MstepAIC.mspe <- rep(NA, kfold)
-MstepBIC.mspe <- rep(NA, kfold)
-
-for (i in 1:kfold) {
-  print(paste0("i = ", i))
-  train.ind <- which(pollutants$index != i)
-
-  MfwdAIC.cv <- update(MfwdAIC, subset=train.ind)
-  MfwdBIC.cv <- update(MfwdBIC, subset=train.ind)
-  MbckAIC.cv <- update(MbckAIC, subset=train.ind)
-  MbckBIC.cv <- update(MbckBIC, subset=train.ind)
-  MstepAIC.cv <- update(MstepAIC, subset=train.ind)
-  MstepBIC.cv <- update(MstepBIC, subset=train.ind)
-
-  i_newdata <- pollutants[-train.ind, ]
-  i_y <- pollutants$length[-train.ind]
-  MfwdAIC.res <- mspe(MfwdAIC.cv, i_newdata, i_y)
-  MfwdBIC.res <- mspe(MfwdBIC.cv, i_newdata, i_y)
-  MbckAIC.res <- mspe(MbckAIC.cv, i_newdata, i_y)
-  MbckBIC.res <- mspe(MbckBIC.cv, i_newdata, i_y)
-  MstepAIC.res <- mspe(MstepAIC.cv, i_newdata, i_y)
-  MstepBIC.res <- mspe(MstepBIC.cv, i_newdata, i_y)
-
-  MfwdAIC.mspe[i] <- MfwdAIC.res
-  MfwdBIC.mspe[i] <- MfwdBIC.res
-  MbckAIC.mspe[i] <- MbckAIC.res
-  MbckBIC.mspe[i] <- MbckBIC.res
-  MstepAIC.mspe[i] <- MstepAIC.res
-  MstepBIC.mspe[i] <- MstepBIC.res
-  
-}
+# basic data manipulation complete
 
 
 #====
-# test the three methods
-pollutants_t <- pollutants_original[-1]
-
-# forward selection using AIC
-fwdAIC.mspe <- rep(NA, kfold)
-for (i in 1:kfold) {
-  train.ind <- which(pollutants$index != i)
-  
-  M0 <- lm(length ~ 1, data=pollutants_t[train.ind, ])
-  Mfull <- lm(length ~ ., data=pollutants_t[train.ind, ])
-  i_MfwdAIC <- step(object = M0, scope = list(lower = M0, upper = Mfull), 
-       trace = FALSE, direction = "forward", k = 2)
-
-  i_newdata <- pollutants_t[-train.ind, ]
-  i_y <- pollutants$length[-train.ind]
-  i_MfwdAIC.res <- mspe(i_MfwdAIC, i_newdata, i_y)
-
-  fwdAIC.mspe[i] <- i_MfwdAIC.res
-}
-
-# backforward elimination using AIC
-bckAIC.mspe <- rep(NA, kfold)
-for (i in 1:kfold) {
-  train.ind <- which(pollutants$index != i)
-  
-  M0 <- lm(length ~ 1, data=pollutants_t[train.ind, ])
-  Mfull <- lm(length ~ ., data=pollutants_t[train.ind, ])
-  i_MbckAIC <- step(object = Mfull, scope = list(lower = M0, upper = Mfull), 
-                    trace = FALSE, direction = "backward", k = 2)
-  
-  i_newdata <- pollutants_t[-train.ind, ]
-  i_y <- pollutants$length[-train.ind]
-  i_MbckAIC.res <- mspe(i_MbckAIC, i_newdata, i_y)
-  
-  bckAIC.mspe[i] <- i_MbckAIC.res
-}
+# test the methods
+kfold <- 12
+pollutants_original$index <- rep(1:kfold, each=N/kfold)
 
 # stepwise selection using AIC
 stepAIC.mspe <- rep(NA, kfold)
 for (i in 1:kfold) {
-  train.ind <- which(pollutants$index != i)
+  print(paste0("i = ", i))
+  train.ind <- which(pollutants_original$index != i)
   
-  M0 <- lm(length ~ 1, data=pollutants_t[train.ind, ])
+  M0 <- lm(length ~ 1, data=pollutants[train.ind, ])
   Mstart <- M0
-  Mfull <- lm(length ~ ., data=pollutants_t[train.ind, ])
+  Mfull <- lm(length ~ ., data=pollutants[train.ind, ])
   i_MstepAIC <- step(object = Mstart, scope = list(lower = M0, upper = Mfull), 
                     trace = FALSE, direction = "both", k = 2)
+  print(row.names(summary(i_MstepAIC)$coefficients))
   
-  i_newdata <- pollutants_t[-train.ind, ]
+  i_newdata <- pollutants[-train.ind, ]
   i_y <- pollutants$length[-train.ind]
   i_MstepAIC.res <- mspe(i_MstepAIC, i_newdata, i_y)
   
   stepAIC.mspe[i] <- i_MstepAIC.res
 }
 
-# forward selection using BIC
-fwdBIC.mspe <- rep(NA, kfold)
-for (i in 1:kfold) {
-  print(paste0("i = ", i))
-  train.ind <- which(pollutants$index != i)
-  
-  M0 <- lm(length ~ 1, data=pollutants_t[train.ind, ])
-  Mfull <- lm(length ~ ., data=pollutants_t[train.ind, ])
-  i_MfwdBIC <- step(object = M0, scope = list(lower = M0, upper = Mfull), 
-                    trace = FALSE, direction = "forward", k = log(length(train.ind)))
-  
-  i_newdata <- pollutants_t[-train.ind, ]
-  i_y <- pollutants$length[-train.ind]
-  i_MfwdBIC.res <- mspe(i_MfwdBIC, i_newdata, i_y)
-  
-  fwdBIC.mspe[i] <- i_MfwdBIC.res
-}
-
-# backforward elimination using AIC
-bckBIC.mspe <- rep(NA, kfold)
-for (i in 1:kfold) {
-  print(paste0("i = ", i))
-  train.ind <- which(pollutants$index != i)
-  
-  M0 <- lm(length ~ 1, data=pollutants_t[train.ind, ])
-  Mfull <- lm(length ~ ., data=pollutants_t[train.ind, ])
-  i_MbckBIC <- step(object = Mfull, scope = list(lower = M0, upper = Mfull), 
-                    trace = FALSE, direction = "backward", k = log(length(train.ind)))
-  
-  i_newdata <- pollutants_t[-train.ind, ]
-  i_y <- pollutants$length[-train.ind]
-  i_MbckBIC.res <- mspe(i_MbckBIC, i_newdata, i_y)
-  
-  bckBIC.mspe[i] <- i_MbckBIC.res
-}
 
 # stepwise selection using BIC
 stepBIC.mspe <- rep(NA, kfold)
 for (i in 1:kfold) {
   print(paste0("i = ", i))
-  train.ind <- which(pollutants$index != i)
+  train.ind <- which(pollutants_original$index != i)
   
-  M0 <- lm(length ~ 1, data=pollutants_t[train.ind, ])
+  M0 <- lm(length ~ 1, data=pollutants[train.ind, ])
   Mstart <- M0
-  Mfull <- lm(length ~ ., data=pollutants_t[train.ind, ])
+  Mfull <- lm(length ~ ., data=pollutants[train.ind, ])
   i_MstepBIC <- step(object = Mstart, scope = list(lower = M0, upper = Mfull), 
                      trace = FALSE, direction = "both", k = log(length(train.ind)))
-  i_newdata <- pollutants_t[-train.ind, ]
+  print(row.names(summary(i_MstepBIC)$coefficients))
+  
+  i_newdata <- pollutants[-train.ind, ]
   i_y <- pollutants$length[-train.ind]
   i_MstepBIC.res <- mspe(i_MstepBIC, i_newdata, i_y)
   
   stepBIC.mspe[i] <- i_MstepBIC.res
 }
-print(paste("fwd AIC MSPE:", mean(fwdAIC.mspe)))
-print(paste("fwd BIC MSPE:", mean(fwdBIC.mspe)))
-print(paste("step AIC MSPE:", mean(stepAIC.mspe)))
-print(paste("bck AIC MSPE:", mean(bckAIC.mspe)))
-print(paste("bck BIC MSPE:", mean(bckBIC.mspe)))
-print(paste("step BIC MSPE:", mean(stepBIC.mspe)))
 
-print(mean(MfwdAIC.mspe))
-print(mean(MfwdBIC.mspe))
-print(mean(MstepAIC.mspe))
-print(mean(MbckAIC.mspe))
-print(mean(MbckBIC.mspe))
-print(mean(MstepBIC.mspe))
+
+covariate_set1 <- c('ageyrs', 'POP_furan3', 'male', 'ln_lbxcot', 'edu_cat', 'monocyte_pct', 'BMI', 'race_cat',
+                   'neutrophils_pct')
+covariate_set11 <- covariate_set1
+covariate_set1 <- c('length', covariate_set1)
+pollutants_set1 <- pollutants[colnames(pollutants) %in% covariate_set1]
+model1 <- lm(length ~ ., data=pollutants_set1)
+
+pollutants_set11 <- pollutants[covariate_set11]
+vif_list11 <- c()
+for (i in 1:length(covariate_set11)) {
+  print(covariate_set11[i])
+  i_model <- lm(as.formula(paste0(covariate_set11[i], "~ .")), data=pollutants_set11)
+  i_vif <- 1 / (1 - summary(i_model)$r.squared)
+  vif_list11 <- c(vif_list11, i_vif)
+  print(paste0("VIF=", i_vif))
+}
+
+covariate_set2 <- c('ageyrs', 'POP_furan3', 'male', 'ln_lbxcot', 'monocyte_pct', 'BMI', 'neutrophils_pct')
+covariate_set2 <- c('length', covariate_set2)
+pollutants_set2 <- pollutants[colnames(pollutants) %in% covariate_set2]
+model2 <- lm(length ~ ., data=pollutants_set2)
+
+# residual analysis
+reslin <- resid(model2) # raw residuals
+studlin <- reslin/(sigma(model2)*sqrt(1-hatvalues(model2))) # studentized residuals
+
+## plot of residuals vs X
+plot(reslin~pollutants_set2$BMI,
+     xlab=expression('BMI'),
+     ylab="Residuals",
+     main="Residuals vs BMI")
+
+# plot of residuals against fitted values
+plot(studlin~fitted(model2),
+     xlab="fitted values",
+     ylab="Residuals",
+     main="Residuals vs Fitted Values")
+
+## plot of residuals vs X^2
+plot(reslin~I(pollutants_set2$BMI^2),
+     xlab=expression("BMI^2"),
+     ylab="Residuals",
+     main="Residuals vs BMI^2")
+
+
+
+# adhoc checks
+model_age_v_edu <- lm(ageyrs ~ edu_cat, data=pollutants_set1)
+summary(model_age_v_edu)
+
+model_age_v_race <- lm(ageyrs ~ race_cat, data=pollutants_set1)
+summary(model_age_v_race)
+
+model_male_v_race <- lm(ageyrs ~ male, data=pollutants_set1)
+summary(model_male_v_race)
+
+model_ageyrs_v_everything <- lm(ageyrs ~ edu_cat + race_cat + male, data=pollutants_set1)
+summary(model_ageyrs_v_everything)
+print(1 / (1 - summary(model_ageyrs_v_everything)$r.squared))
+vif(model_ageyrs_v_everything)
+
+summary(lm(edu_cat ~ ageyrs + race_cat + male, data=pollutants_original))
